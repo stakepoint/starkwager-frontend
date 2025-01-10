ddc723cea9f6779a97912c9cb05855784074d153
Adeyemi Gbenga <addegbenga@gmail.com>
2025-01-10 17:11:48 +0100

    feat: added skip button re-init repo with wallet_connect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Button, buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import React from "react";
