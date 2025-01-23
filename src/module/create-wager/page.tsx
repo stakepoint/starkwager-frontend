13ad3ababbb19e7b26f8bb3d6486c5d1f4d5d4a9
ahmad <abdulkareemahmad413@gmail.com>
2025-01-23 22:51:40 +0100

    replaced react-icons with lucide-icon

--- src/module/create-wager/page.tsx
+++ src/module/create-wager/page.tsx
@@ -1,11 +1,10 @@
-import DashboardNavbar from "@/components/layouts/dashboardNavbar";
 import React from "react";
-import { TiArrowSortedDown } from "react-icons/ti";
+import { ChevronDown } from "lucide-react";
 
 export default function CreateWager() {
   return (
     <div>
-      {/* <DashboardNavbar /> */}
+      
       <div className='flex justify-center p-5 mt-10 items-center'>
         <form className='text-[#102A56] w-full sm:w-[90%] md:w-[75%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%] mx-auto'>
           <div className='grid grid-cols-2 gap-3 mb-5'>
@@ -14,13 +13,13 @@ export default function CreateWager() {
               <h3 className='md:text-sm text-[.75rem] text-[#102A56]'>
                 Select Category
               </h3>
-              <TiArrowSortedDown className='cursor-pointer text-md' />
+              <ChevronDown  className='cursor-pointer text-md' />
             </div>
 
             {/*Select Add Hash tags field*/}
             <div className='flex items-center bg-[#EFF1F5] px-3 py-5 rounded-sm justify-between'>
               <h3 className='md:text-sm text-[.75rem]'>Add HashTags</h3>
-              <TiArrowSortedDown className='cursor-pointer text-lg' />
+              <ChevronDown className='cursor-pointer text-lg' />
             </div>
           </div>
 
