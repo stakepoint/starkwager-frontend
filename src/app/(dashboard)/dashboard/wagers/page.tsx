ce9cd3282deada428dbb940fe483ccb3c9f34dc5
bigchills <yemi7164@gmail.com>
2025-02-23 16:19:07 +0100

    feat:add eloading states and smooth animations

--- src/app/(dashboard)/dashboard/wagers/page.tsx
+++ src/app/(dashboard)/dashboard/wagers/page.tsx
@@ -1,72 +1,107 @@
+"use client";
+import { useState, useEffect } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import WagerCards from "@/components/ui/WagerCards";
+import WagerCardSkeleton from "@/components/ui/skeletons/wagerCardSkeleton";
 
 export default function Wagers() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
-    <div className="w-full pt-10 pb-20 md:py-20 flex justify-center items-center ">
-      <Tabs defaultValue="account" className="w-[650px]">
+    <div className="w-full pt-10 pb-20 md:py-20 flex justify-center items-center">
+      <Tabs defaultValue="active" className="w-[650px]">
         <TabsList className="flex justify-center">
           <TabsTrigger value="active">Active</TabsTrigger>
           <TabsTrigger value="pending">Pending</TabsTrigger>
           <TabsTrigger value="complete">Complete</TabsTrigger>
         </TabsList>
+
         <TabsContent value="active">
-          <WagerCards
-            wagerStatus="active"
-            question="Will Bitcoin Hit $100k Before January 31, 2025?"
-            stakeAmount={100}
-            leftUser={{
-              username: "@noyi24_7",
-              icon: "/images/leftWagercardUserOneIcon.svg",
-            }}
-            rightUser={{
-              username: "@babykeem",
-              icon: "/images/rightWagercardUserOneIcon.svg",
-            }}
-          />
-          <WagerCards
-            wagerStatus="active"
-            question="Will Bitcoin Hit $100k Before January 31, 2025?"
-            stakeAmount={100}
-            leftUser={{
-              username: "@noyi24_7",
-              icon: "/images/leftWagercardUserOneIcon.svg",
-            }}
-            rightUser={{
-              username: "@babykeem",
-              icon: "/images/rightWagercardUserOneIcon.svg",
-            }}
-          />
+          {loading ? (
+            <div className="space-y-3">
+              <WagerCardSkeleton />
+              <WagerCardSkeleton />
+            </div>
+          ) : (
+            <>
+              <WagerCards
+                wagerId=""
+                wagerStatus="active"
+                question="Will Bitcoin Hit $100k Before January 31, 2025?"
+                stakeAmount={100}
+                leftUser={{
+                  username: "@noyi24_7",
+                  icon: "/images/leftWagercardUserOneIcon.svg",
+                }}
+                rightUser={{
+                  username: "@babykeem",
+                  icon: "/images/rightWagercardUserOneIcon.svg",
+                }}
+              />
+              <WagerCards
+                wagerId=""
+                wagerStatus="active"
+                question="Will Bitcoin Hit $100k Before January 31, 2025?"
+                stakeAmount={100}
+                leftUser={{
+                  username: "@noyi24_7",
+                  icon: "/images/leftWagercardUserOneIcon.svg",
+                }}
+                rightUser={{
+                  username: "@babykeem",
+                  icon: "/images/rightWagercardUserOneIcon.svg",
+                }}
+              />
+            </>
+          )}
         </TabsContent>
+
         <TabsContent value="pending">
-          <WagerCards
-            wagerStatus="pending"
-            question="Will Bitcoin Hit $100k Before January 31, 2025?"
-            stakeAmount={100}
-            leftUser={{
-              username: "@noyi24_7",
-              icon: "/images/leftWagercardUserOneIcon.svg",
-            }}
-            rightUser={{
-              username: "Awaiting Opponent",
-              icon: "/images/opponent.svg",
-            }}
-          />
+          {loading ? (
+            <WagerCardSkeleton />
+          ) : (
+            <WagerCards
+              wagerId=""
+              wagerStatus="pending"
+              question="Will Bitcoin Hit $100k Before January 31, 2025?"
+              stakeAmount={100}
+              leftUser={{
+                username: "@noyi24_7",
+                icon: "/images/leftWagercardUserOneIcon.svg",
+              }}
+              rightUser={{
+                username: "Awaiting Opponent",
+                icon: "/images/opponent.svg",
+              }}
+            />
+          )}
         </TabsContent>
+
         <TabsContent value="complete">
-          <WagerCards
-            wagerStatus="completed"
-            question="Will Bitcoin Hit $100k Before January 31, 2025?"
-            stakeAmount={100}
-            leftUser={{
-              username: "@noyi24_7",
-              icon: "/images/leftWagercardUserOneIcon.svg",
-            }}
-            rightUser={{
-              username: "@babykeem",
-              icon: "/images/rightWagercardUserOneIcon.svg",
-            }}
-          />
+          {loading ? (
+            <WagerCardSkeleton />
+          ) : (
+            <WagerCards
+              wagerId=""
+              wagerStatus="completed"
+              question="Will Bitcoin Hit $100k Before January 31, 2025?"
+              stakeAmount={100}
+              leftUser={{
+                username: "@noyi24_7",
+                icon: "/images/leftWagercardUserOneIcon.svg",
+              }}
+              rightUser={{
+                username: "@babykeem",
+                icon: "/images/rightWagercardUserOneIcon.svg",
+              }}
+            />
+          )}
         </TabsContent>
       </Tabs>
     </div>
