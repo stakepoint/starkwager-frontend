6caa4a4b7ce2526c84ba9fed6f7e1f79deecc361
bigchills <yemi7164@gmail.com>
2025-02-23 14:04:03 +0100

    feat: update active status

--- src/app/(dashboard)/dashboard/wagers/page.tsx
+++ src/app/(dashboard)/dashboard/wagers/page.tsx
@@ -3,7 +3,7 @@ import WagerCards from "@/components/ui/WagerCards";
 
 export default function Wagers() {
   return (
-    <div className="w-full pt-20 flex justify-center items-center">
+    <div className="w-full pt-10 pb-20 md:py-20 flex justify-center items-center ">
       <Tabs defaultValue="account" className="w-[650px]">
         <TabsList className="flex justify-center">
           <TabsTrigger value="active">Active</TabsTrigger>
@@ -25,7 +25,7 @@ export default function Wagers() {
             }}
           />
           <WagerCards
-            wagerStatus="pending"
+            wagerStatus="active"
             question="Will Bitcoin Hit $100k Before January 31, 2025?"
             stakeAmount={100}
             leftUser={{
