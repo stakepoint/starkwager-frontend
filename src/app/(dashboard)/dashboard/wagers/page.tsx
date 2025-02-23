32bf0c40cd84dbe5ab632228b090353b57c6065f
bigchills <yemi7164@gmail.com>
2025-02-23 11:37:15 +0100

    feat:implement tabs for wagers page

--- src/app/(dashboard)/dashboard/wagers/page.tsx
+++ src/app/(dashboard)/dashboard/wagers/page.tsx
@@ -1,7 +1,59 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import WagerCards from "@/components/ui/WagerCards";
+
 export default function Wagers() {
-    return (
-        <div className="w-full">
-            <h1>Wagers Page</h1>
-        </div>
-    )
+  return (
+    <div className="w-full pt-20 flex justify-center items-center">
+      <Tabs defaultValue="account" className="w-[650px]">
+        <TabsList className="flex justify-center">
+          <TabsTrigger value="active">Active</TabsTrigger>
+          <TabsTrigger value="pending">Pending</TabsTrigger>
+          <TabsTrigger value="complete">Complete</TabsTrigger>
+        </TabsList>
+        <TabsContent value="active">
+          <WagerCards
+            question="Will Bitcoin Hit $100k Before January 31, 2025?"
+            progress={true}
+            stakeAmount={100}
+            leftUser={{
+              username: "@noyi24_7",
+              icon: "/images/leftWagercardUserOneIcon.svg",
+            }}
+            rightUser={{
+              username: "@babykeem",
+              icon: "/images/rightWagercardUserOneIcon.svg",
+            }}
+          />
+          <WagerCards
+            question="Will Bitcoin Hit $100k Before January 31, 2025?"
+            progress={true}
+            stakeAmount={100}
+            leftUser={{
+              username: "@noyi24_7",
+              icon: "/images/leftWagercardUserOneIcon.svg",
+            }}
+            rightUser={{
+              username: "@babykeem",
+              icon: "/images/rightWagercardUserOneIcon.svg",
+            }}
+          />
+        </TabsContent>
+        <TabsContent value="pending">
+          <WagerCards
+            question="Will Bitcoin Hit $100k Before January 31, 2025?"
+            progress={false}
+            stakeAmount={100}
+            leftUser={{
+              username: "@noyi24_7",
+              icon: "/images/leftWagercardUserOneIcon.svg",
+            }}
+            rightUser={{
+              username: "Awaiting Opponent",
+              icon: "/images/opponent.svg",
+            }}
+          />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 }
