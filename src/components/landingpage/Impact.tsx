bfc521c9f8bb5b4d42a4a2f84d25bbc228efce8b
RareMmemshima <Ihuwediane@gmail.com>
2025-03-23 13:22:04 +0000

    Mar 23, 2025, 2:22 PM

---
+++ src/components/landingpage/Impact.tsx
@@ -0,0 +1,73 @@
+import Image from "next/image";
+
+const countWithText = [
+  { count: "50", text: "contributors on onlydust" },
+  { count: "10,000", text: "wagers placed onchain" },
+  { count: "5,000", text: "strk tokens distributed fairly" },
+];
+
+const Impact = () => {
+  return (
+    <div className="flex flex-col max-w-3xl mx-auto gap-5 text-left uppercase">
+      <p className=" text-[#E0FE10] -rotate-2 px-2 font-comedik">
+        the impact so far;
+      </p>
+      <div className="flex flex-col md:flex-row justify gap-4 -between px-2 my-4">
+        {countWithText.map(({ count, text }) => (
+          <div key={text} className="flex flex-col text-left">
+            <span className="text-6xl font-bold font-schabo">{count}+</span>
+            <span className="flex flex-wrap text-ellipsis w-2/3 font-semibold  text-sm">
+              {text}
+            </span>
+          </div>
+        ))}
+      </div>
+      <div>
+        <Image
+          src="/images/STRIP.png"
+          width={1000}
+          height={200}
+          alt="strkwager tldr"
+          className="w-full h-full object-cover md:object-contain object-center rounded-md"
+        />
+      </div>
+      <p className="text-xl uppercase text-[#E0FE10] -rotate-2 px-2  text-ellipsis max-w-sm font-comedik">
+        you&apos;ve made it this far, so here&apos;s the tl;dr:
+      </p>
+      <p className="flex flex-wrap w-3/4 lowercase font-medium text-wrap px-2">
+        Strkwager is here to make betting transparent, fun and trustless. No
+        middleman, no hidden fees - just fair wagering for everyone.
+      </p>
+      <Image
+        src="/images/right-arrow.png"
+        width={100}
+        height={100}
+        alt="strkwager tldr"
+        className="object-contain md:hidden absolute top-3/4 right-0 transform -translate-x-7 translate-y-16"
+      />
+      <Image
+        src="/images/left-arrow.png"
+        width={100}
+        height={100}
+        alt="strkwager tldr"
+        className="object-contain hidden md:block absolute top-3/4 -translate-x-2/3"
+      />
+      {/* eft-0 top-0 transform -translate-x-10 -translate-y-10 */}
+      <div className="flex px-2 md:flex-row w-full gap-10 mr-10 py-5 text-left">
+        <div className="max-w-5xl font-schabo">
+          <p className="text-3xl md:text-5xl font-bold">ready to</p>
+          <p className="text-2xl md:text-5xl font-bold">place your</p>
+          <p className="text-2xl md:text-5xl font-bold">first wager?</p>
+        </div>
+        <div className="flex flex-col gap-5">
+          <p className="text-[#E0FE10] w-1/2 -rotate-2 font-comedik">
+            were open to contributors and players alike
+          </p>
+          <p className="text-[#E0FE10] text-4xl font-comedik">lets connect</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Impact;
